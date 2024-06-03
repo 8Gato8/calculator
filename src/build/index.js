fb354db1514fca34d348d87d02e0e99273434504
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-06-03 20:57:19 +0800

    feat: add basic math operations functions

--- src/build/index.js
+++ src/build/index.js
@@ -1,1 +1,5 @@
-
+"use strict";
+function add(...args) {
+    return args.reduce((prev, current) => prev + current);
+}
+console.log(add(2, 4, 7, 6));
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,17 @@
+type TBasicOperation = (first: number, second: number) => number;
 
+const add: TBasicOperation = function (firstSummand, secondSummand) {
+  return firstSummand + secondSummand;
+};
+
+const subtract: TBasicOperation = function (min: number, sub: number) {
+  return min - sub;
+};
+
+const multiply: TBasicOperation = function (multiplier: number, multiplicand: number) {
+  return multiplier * multiplicand;
+};
+
+const divide: TBasicOperation = function (divident: number, divisor: number) {
+  return divident / divisor;
+};
